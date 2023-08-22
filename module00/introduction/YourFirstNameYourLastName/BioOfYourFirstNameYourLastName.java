064e52cc78af7b0ed706b69bfe9b6a89d5dba540
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-08-22 07:46:22 -0500

    added bio

---
+++ module00/introduction/YourFirstNameYourLastName/BioOfYourFirstNameYourLastName.java
@@ -0,0 +1,8 @@
+package module00.introduction.YourFirstNameYourLastName;
+
+public class BioOfYourFirstNameYourLastName {
+    public static void main(String[] args) {
+        System.out.println(" My name is Jeffrey Cabrera, I have aspirations to transfer to the University of Houston and pursue a degree in Computer Science.\n " + "I keep myself busy by learning new programming languages. \n"+" Fitness is important to me, and I make it a point to hit the gym whenever possible.\n"+" During my time in the Marines, I was stationed in California and had the opportunity to travel to Okinawa, Japan. \n"+" Outside of my studies and work, I also do food deliveries for Uber, DoorDash, and Grubhub.");
+    }
+
+}
