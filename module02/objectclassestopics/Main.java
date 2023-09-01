a4af82bb6d3590fa1303050a9d19c9847f9312d3
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-08-31 21:42:04 -0500

    added main method

--- module02/objectclassestopics/Main.java
+++ module02/objectclassestopics/Main.java
@@ -4,9 +4,66 @@ public class Main {
 	public static void main(String[] args) {
 		PersonArray persons = new PersonArray(100); // for example, an array of 100 persons
 
-		// Code to add, display, update, delete persons, using the methods from the PersonArray class
+		// Adding 50 random persons to the array
+		for (int i = 0; i < 50; i++) {
+			Person randomPerson = new Person(
+					(long) i,  // Using 'i' as ID just for demonstration purposes
+					MyRandomPersonData.getRandomFirstName(),
+					MyRandomPersonData.getRandomLastName(),
+					MyRandomPersonData.getRandomStreetNo(),
+					MyRandomPersonData.getRandomStreetName(),
+					MyRandomPersonData.getRandomCity(),
+					MyRandomPersonData.getRandomState(),
+					MyRandomPersonData.getRandomZip(),
+					MyRandomPersonData.getRandomPhone(),
+					MyRandomPersonData.getRandomSalary(),
+					MyRandomPersonData.getRandomCreationDate(),
+					MyRandomPersonData.getRandomIsMember(),
+					MyRandomPersonData.getRandomPassword()
+			);
+			persons.addPerson(randomPerson);
+		}
 
-		// Display the persons, demonstrate other functionalities
+		// Displaying all persons
+		System.out.println("Displaying all persons:");
+		persons.displayPersons();
+		System.out.println("\nTotal number of persons: " + persons.getSize() + "\n");
+
+		// Updating a person's data
+		Long idToUpdate = 10L; // Let's say we want to update the person with ID 10
+		Person updatedPerson = new Person(
+				idToUpdate,
+				"UpdatedFirstName",
+				"UpdatedLastName",
+				(short) 999,
+				"UpdatedStreet",
+				"UpdatedCity",
+				"UpdatedState",
+				99999,
+				9999999999L,
+				99999.99F,
+				MyRandomPersonData.getRandomCreationDate(),
+				false,
+				"UpdatedPassword"
+		);
+		if (persons.updatePerson(idToUpdate, updatedPerson)) {
+			System.out.println("Updated the person with ID: " + idToUpdate);
+		} else {
+			System.out.println("Couldn't find the person with ID: " + idToUpdate);
+		}
+
+		// Deleting a person
+		Long idToDelete = 25L; // Let's say we want to delete the person with ID 25
+		if (persons.deletePerson(idToDelete)) {
+			System.out.println("Deleted the person with ID: " + idToDelete);
+		} else {
+			System.out.println("Couldn't find the person with ID: " + idToDelete);
+		}
+
+		// Displaying all persons after updates
+		System.out.println("\nDisplaying all persons after updates:");
+		persons.displayPersons();
+		System.out.println("\nTotal number of persons after updates: " + persons.getSize());
 	}
 }
 
--- module02/objectclassestopics/MyRandomPersonData.java
+++ module02/objectclassestopics/MyRandomPersonData.java
@@ -1,15 +1,71 @@
 package module02.objectclassestopics;
 
+import java.util.Date;
 import java.util.Random;
 
 public class MyRandomPersonData {
+
     private static final Random random = new Random();
-    private static final String[] FIRST_NAMES = {"John", "Jane", "Sam", /* ... other names ... */};
-    // similarly, arrays for last names, street names, cities, states etc.
+
+    private static final String[] FIRST_NAMES = {"John", "Jane", "Sam", "Lucy", "Michael", "Sarah"};
+    private static final String[] LAST_NAMES = {"Smith", "Doe", "Johnson", "Williams", "Brown", "Jones"};
+    private static final String[] STREET_NAMES = {"Main St", "High St", "Park Ave", "Elm St", "2nd St"};
+    private static final String[] CITIES = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix"};
+    private static final String[] STATES = {"NY", "CA", "IL", "TX", "AZ"};
 
     public static String getRandomFirstName() {
         return FIRST_NAMES[random.nextInt(FIRST_NAMES.length)];
     }
 
-    // similarly, methods for last name, street name, city, state, zip etc.
+    public static String getRandomLastName() {
+        return LAST_NAMES[random.nextInt(LAST_NAMES.length)];
+    }
+
+    public static String getRandomStreetName() {
+        return STREET_NAMES[random.nextInt(STREET_NAMES.length)];
+    }
+
+    public static short getRandomStreetNo() {
+        return (short) (random.nextInt(1000) + 1);
+    }
+
+    public static String getRandomCity() {
+        return CITIES[random.nextInt(CITIES.length)];
+    }
+
+    public static String getRandomState() {
+        return STATES[random.nextInt(STATES.length)];
+    }
+
+    public static int getRandomZip() {
+        return random.nextInt(90000) + 10000;  // Zip codes from 10000 to 99999
+    }
+
+    public static long getRandomPhone() {
+        return 1000000000L + random.nextInt(900000000);  // 10 digit phone number
+    }
+
+    public static float getRandomSalary() {
+        return random.nextFloat() * 100000;  // Salary between 0.00 to 100000.00
+    }
+
+    public static Date getRandomCreationDate() {
+        // Generate random dates in the last 10 years
+        long tenYearsInMillis = 10L * 365 * 24 * 60 * 60 * 1000;
+        return new Date(System.currentTimeMillis() - random.nextInt((int) tenYearsInMillis));
+    }
+
+    public static boolean getRandomIsMember() {
+        return random.nextBoolean();
+    }
+
+    public static String getRandomPassword() {
+        // Here, I'm generating a random 8 character password (simple example, might not be secure)
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            password.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return password.toString();
+    }
 }
