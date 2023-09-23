5f969195e1dd3a7e46b2f1a05f7e2eb95e4f5bee
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-09-22 20:01:39 -0500

    added comments

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.print("Welcome to Java \n"  + "Welcome to Computer Science \n" + "Programming is fun");
+
     }
 }
